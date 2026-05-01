8ceb8101d20dbc271ff15acba731b61ea3d38da1
kurniawansyahrifky-stack <kurniawansyahrifky@gmail.com>
2026-05-02 03:47:12 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+API_ID = 33370509
+API_HASH = "669af6caebf2aca264b16cf8b40d37b2"
+BOT_TOKEN = "ISI_TOKEN_LU"
+
+API_URL = "http://127.0.0.1:5000/get"
+
+TARGET_CHATS = [-1002896045673]
+FORCE_GROUP = -1002896045673
+FORCE_LINK = "https://t.me/ofcnoirfleur"
+
+OWNER_IDS = [8209644174, 5744453710]
+
+PARTNER_FILE = "partner.json"
+SETTING_FILE = "setting.json"
+QUEUE_FILE = "queue.json"
+AUTO_TAG_FILE = "autotag.json"
+BUTTON_FILE = "buttons.json"
+
+BACKUP_DIR = "backups"
+SERVICE_NAME = "noirbot"
