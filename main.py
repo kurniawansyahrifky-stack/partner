edb686e70341538bd21c2c013104bb55dbf57dd5
kurniawansyahrifky-stack <kurniawansyahrifky@gmail.com>
2026-05-05 00:42:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import asyncio
+from pyrogram import idle
 from client import app
 
 from modules.start import start_cmd
 from modules.tagall import tagall_cmd, cancel_cmd
 from modules.partner_list import list_partner
-from modules.owner import *
-from modules.owner_panel import *
 from modules.button_handler import button_handler
 from modules.edit_and_button import handle_edit
 
@@ -23,9 +23,8 @@ async def main():
     await app.start()
     print("🚀 BOT RUNNING...")
     await idle()
+    await app.stop()
 
 
 if __name__ == "__main__":
-    import asyncio
-    from pyrogram import idle
     asyncio.run(main())
