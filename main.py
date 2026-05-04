8a7b24f7f55509f4db6d2b5e2d6ab58860607bbf
kurniawansyahrifky-stack <kurniawansyahrifky@gmail.com>
2026-05-05 00:43:58 +0700

    Refactor handler registration in main.py
    Refactored handler registration by importing modules directly.

--- main.py
+++ main.py
@@ -2,24 +2,14 @@ import asyncio
 from pyrogram import idle
 from client import app
 
-from modules.start import start_cmd
-from modules.tagall import tagall_cmd, cancel_cmd
-from modules.partner_list import list_partner
-from modules.button_handler import button_handler
-from modules.edit_and_button import handle_edit
-
-
-def register_handlers():
-    app.add_handler(start_cmd)
-    app.add_handler(tagall_cmd)
-    app.add_handler(cancel_cmd)
-    app.add_handler(list_partner)
-    app.add_handler(button_handler)
-    app.add_handler(handle_edit)
+import modules.start
+import modules.tagall
+import modules.partner_list
+import modules.button_handler
+import modules.edit_and_button
 
 
 async def main():
-    register_handlers()
     await app.start()
     print("🚀 BOT RUNNING...")
     await idle()
