cc5e6a00298a6eb2ab666e7716c44aafeffdec61
kurniawansyahrifky-stack <kurniawansyahrifky@gmail.com>
2026-05-05 01:08:43 +0700

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,9 @@
+# modules package initializer
+
+from . import start
+from . import tagall
+from . import partner_list
+from . import button_handler
+from . import edit_and_button
+
+print("📦 Modules loaded successfully")
